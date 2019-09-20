00abebc102a74cd8e3904e229e906d6fb2290372
bspark <erlk0nig@naver.com>
2019-09-20 17:47:54 +0900

    add logging feature

--- memtier-json.py
+++ memtier-json.py
@@ -1,9 +1,12 @@
 import os, sys, json
+from datetime import datetime
+
+MAKE_LOG = True
 
 json_path = "./conf/memtier-sample.json" if len(sys.argv) == 1 else sys.argv[1]
 json_data = json.loads(open(json_path).read())
 
-memtier_path= "./memtier_benchmark"
+memtier_path= "~/memtier_benchmark/memtier_benchmark"
 cmd = memtier_path
 
 for key, value in json_data.items():
@@ -17,4 +20,9 @@ for key, value in json_data.items():
         
     cmd += value
 
+if MAKE_LOG:
+    d = datetime.today()
+    cmd += " > " + "log/" + d.strftime("%Y-%m-%d-%H:%M:%S")
+
 os.system(cmd)
+print(cmd)
